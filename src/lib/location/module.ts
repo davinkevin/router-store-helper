5781fa60cab40cbc5bcb940ef65a0e5aaf46e92a
davinkevin <davin.kevin@gmail.com>
2018-02-11 00:33:07 +0100

    fix(module): exports effects

--- src/lib/location/module.ts
+++ src/lib/location/module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {EffectsModule} from '@ngrx/effects';
 import {LocationEffects} from './location.effects';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
     EffectsModule.forFeature([LocationEffects])
-  ]
+  ],
+  exports: [EffectsModule]
 })
-export class LocationStoreHelper {}
+export class LocationStoreHelperModule {}
--- src/lib/router/module.ts
+++ src/lib/router/module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {EffectsModule} from '@ngrx/effects';
 import {RouterEffects} from './router.effects';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
     EffectsModule.forFeature([RouterEffects])
-  ]
+  ],
+  exports: [EffectsModule]
 })
-export class RouterStoreHelper {}
+export class RouterStoreHelperModule {}
