7cd54564a4ad2b01d0bd58f6823324c3afb45d2e
davinkevin <davin.kevin@gmail.com>
2018-02-11 01:17:31 +0100

    feat(serializer): add simple router state serializer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ export * from './location/location.actions';
 export * from './location/location.effects';
 export * from './location/module';
 
+export * from './router/simple-router.serializer';
 export * from './router/router.actions';
 export * from './router/router.effects';
 export * from './router/module';
--- src/lib/router/module.ts
+++ src/lib/router/module.ts
@@ -2,11 +2,16 @@ import {NgModule} from '@angular/core';
 import {EffectsModule} from '@ngrx/effects';
 import {RouterEffects} from './router.effects';
 import {CommonModule} from '@angular/common';
+import {RouterStateSerializer} from '@ngrx/router-store';
+import {SimpleRouterStateSerializer} from './simple-router.serializer';
 
 @NgModule({
   imports: [
     CommonModule,
     EffectsModule.forFeature([RouterEffects])
+  ],
+  providers: [
+    { provide: RouterStateSerializer, useClass: SimpleRouterStateSerializer }
   ],
   exports: [EffectsModule]
 })
