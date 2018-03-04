063656f2d3b77490c070bfeb66d18a73c09995dc
davinkevin <davin.kevin@gmail.com>
2018-03-04 13:54:44 +0100

    refactor(router): used destructuring when possible to simplify syntax

--- src/lib/router/router.effects.ts
+++ src/lib/router/router.effects.ts
@@ -12,14 +12,14 @@ export class RouterEffects {
   navigate$: Observable<RouterNavigateAction> = this.actions$
     .pipe(
       ofType(RouterActions.navigate),
-      tap((a: RouterNavigateAction) => this.router.navigate(a.commands, a.extras))
+      tap(({commands, extras}: RouterNavigateAction) => this.router.navigate(commands, extras))
     );
 
   @Effect({dispatch: false})
   navigateByUrl$: Observable<RouterNavigateByUrlAction> = this.actions$
     .pipe(
       ofType(RouterActions.navigateByUrl),
-      tap((a: RouterNavigateByUrlAction) => this.router.navigateByUrl(a.url, a.extras))
+      tap(({url, extras}: RouterNavigateByUrlAction) => this.router.navigateByUrl(url, extras))
     );
 
   constructor(private actions$: Actions, private router: Router) {}
