4424f172c59b18b2c583edc2d44282224d9a602c
Dji <Dji75@users.noreply.github.com>
2019-01-03 16:02:53 +0100

    chore(ngrx): upgrade ngrx/angular to v7
    BREAKING CHANGE:
      SimpleRouterStateSerializer should be set as default router store serializer in StoreRouterConnectingModule initialization :
        StoreRouterConnectingModule.forRoot({
          serializer: SimpleRouterStateSerializer,
        }),

--- projects/davinkevin/router-store-helper/src/lib/router/module.ts
+++ projects/davinkevin/router-store-helper/src/lib/router/module.ts
@@ -1,17 +1,13 @@
 import {NgModule} from '@angular/core';
 import {EffectsModule} from '@ngrx/effects';
-import {RouterEffects} from './router.effects';
 import {CommonModule} from '@angular/common';
-import {RouterStateSerializer} from '@ngrx/router-store';
-import {SimpleRouterStateSerializer} from './simple-router.serializer';
+
+import {RouterEffects} from './router.effects';
 
 @NgModule({
   imports: [
     CommonModule,
     EffectsModule.forFeature([RouterEffects])
-  ],
-  providers: [
-    { provide: RouterStateSerializer, useClass: SimpleRouterStateSerializer }
   ],
   exports: [EffectsModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import {StoreModule} from '@ngrx/store';
 import {routerReducer, StoreRouterConnectingModule} from '@ngrx/router-store';
-import {LocationStoreHelperModule, RouterStoreHelperModule} from '@davinkevin/router-store-helper';
 import {EffectsModule} from '@ngrx/effects';
+
+import {LocationStoreHelperModule, RouterStoreHelperModule, SimpleRouterStateSerializer} from '@davinkevin/router-store-helper';
+
+import {AppComponent} from './app.component';
 import {LocationComponent} from './location/location.component';
 import {RouterComponent} from './router/router.component';
 import {HomeComponent} from './home/home.component';
@@ -30,14 +31,13 @@ const routes: Routes = [
       router: routerReducer,
     }),
     StoreRouterConnectingModule.forRoot({
-      stateKey: 'router', // name of reducer key
+      serializer: SimpleRouterStateSerializer,
     }),
     EffectsModule.forRoot([]),
     BrowserModule,
     RouterStoreHelperModule,
     LocationStoreHelperModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
